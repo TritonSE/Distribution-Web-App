0f438898d79f57d7a0d7a6d285e295b6cb079b55
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2021-02-01 21:49:40 -0800

    added comments to navbar tests

--- __tests__/navbar.test.js
+++ __tests__/navbar.test.js
@@ -5,12 +5,13 @@ import { render, screen } from '@testing-library/react';
 import Navbar from '../src/components/Navbar/Navbar'
 import "@testing-library/jest-dom/extend-expect";
 
+//links that will be checked for in the navbar
 const links = [
     { text: 'Home', location: "/" },
     { text: 'Agency', location: "/agency" },
 ];
 
-// Use test.each to iterate the test cases above
+// Iterating through above test cases to test if navbar has correct links
 test.each(links)(
     "Check if Nav Bar have %s link.",
     (link) => {
@@ -19,6 +20,8 @@ test.each(links)(
         <Navbar />
       </BrowserRouter>);
       const linkDom = screen.getByText(link.text);
+
+      //toHaveAttrbitute is JEST assertion to check if links are correct
       expect(linkDom).toHaveAttribute("href", link.location);
     }
 );
